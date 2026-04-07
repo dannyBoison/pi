44d66da16975c0c3468deb9ea959233728f98c00
Daniel Boison <110826248+dannyBoison@users.noreply.github.com>
2026-04-07 04:34:42 +0000

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,9 +6,7 @@ export default function Header() {
       <h1>Pilot Weather & Terrain Safety Advisor</h1>
       <nav>
         <button>Home</button>
-        <button>Map</button>
-        <button>Flight Status</button>
-        <button>Training</button>
+        
       </nav>
     </header>
   );
